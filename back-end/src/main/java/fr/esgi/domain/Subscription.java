9e30fdf43efcd21590dbdd7b419caad1953a91f7
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-04-14 00:34:01 +0200

    [fix conflict]

--- back-end/src/main/java/fr/esgi/domain/Subscription.java
+++ back-end/src/main/java/fr/esgi/domain/Subscription.java
@@ -1,12 +1,7 @@
 package fr.esgi.domain;
 
 import javax.persistence.*;
-<<<<<<< Updated upstream
-import java.util.Date;
-=======
 import java.time.LocalDate;
->>>>>>> Stashed changes
-import java.util.Objects;
 
 @Entity
 public class Subscription {
@@ -15,22 +10,11 @@ public class Subscription {
     private Long id;
 
     @ManyToOne
-<<<<<<< Updated upstream
-    @JoinColumn(name = "category_id_subscription")
-    private Category category;
-
-    @Temporal(TemporalType.DATE)
-    private Date subscriptionDate;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id_subscription")
-=======
     private Category category;
 
     private LocalDate subscriptionDate;
 
     @ManyToOne
->>>>>>> Stashed changes
     private User user;
 
     public Subscription() { }
@@ -51,19 +35,11 @@ public class Subscription {
         this.category = category;
     }
 
-<<<<<<< Updated upstream
-    public Date getSubscriptionDate() {
-        return subscriptionDate;
-    }
-
-    public void setSubscriptionDate(Date subscriptionDate) {
-=======
     public LocalDate getSubscriptionDate() {
         return subscriptionDate;
     }
 
     public void setSubscriptionDate(LocalDate subscriptionDate) {
->>>>>>> Stashed changes
         this.subscriptionDate = subscriptionDate;
     }
 
@@ -73,21 +49,5 @@ public class Subscription {
 
     public void setUser(User user) {
         this.user = user;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Subscription that = (Subscription) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(category, that.category) &&
-                Objects.equals(subscriptionDate, that.subscriptionDate) &&
-                Objects.equals(user, that.user);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, category, subscriptionDate, user);
     }
 }
--- back-end/src/main/java/fr/esgi/domain/User.java
+++ back-end/src/main/java/fr/esgi/domain/User.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.Instant;
 import java.util.*;
 
 
@@ -45,6 +44,10 @@ public class User {
     private String email;
 
     @NotNull
+    @Size(min = 5, max = 100)
+    private String countryOfResidence;
+
+    @NotNull
     @Column(nullable = false)
     private boolean activated = false;
 
@@ -56,19 +59,6 @@ public class User {
     @Column(name = "image_url", length = 256)
     private String imageUrl;
 
-    @Size(max = 20)
-    @Column(name = "activation_key", length = 20)
-    @JsonIgnore
-    private String activationKey;
-
-    @Size(max = 20)
-    @Column(name = "reset_key", length = 20)
-    @JsonIgnore
-    private String resetKey;
-
-    @Column(name = "reset_date")
-    private Instant resetDate = null;
-
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private Set<Subscription> subscriptions = new HashSet<>();
 
@@ -79,6 +69,9 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "authority_id")
     private Authority authority = new Authority();
+
+
+    public User() { }
 
     public Long getId() {
         return id;
@@ -145,28 +138,16 @@ public class User {
         this.activated = activated;
     }
 
-    public String getActivationKey() {
-        return activationKey;
+    public String getCountryOfResidence() {
+        return countryOfResidence;
     }
 
-    public void setActivationKey(String activationKey) {
-        this.activationKey = activationKey;
+    public void setCountryOfResidence(String countryOfResidence) {
+        this.countryOfResidence = countryOfResidence;
     }
 
-    public String getResetKey() {
-        return resetKey;
-    }
-
-    public void setResetKey(String resetKey) {
-        this.resetKey = resetKey;
-    }
-
-    public Instant getResetDate() {
-        return resetDate;
-    }
-
-    public void setResetDate(Instant resetDate) {
-        this.resetDate = resetDate;
+    public boolean isActivated() {
+        return activated;
     }
 
     public String getLangKey() {
@@ -175,10 +156,6 @@ public class User {
 
     public void setLangKey(String langKey) {
         this.langKey = langKey;
-    }
-
-    public boolean isActivated() {
-        return activated;
     }
 
     public Set<Subscription> getSubscriptions() {
@@ -217,11 +194,9 @@ public class User {
                 Objects.equals(firstName, user.firstName) &&
                 Objects.equals(lastName, user.lastName) &&
                 Objects.equals(email, user.email) &&
+                Objects.equals(countryOfResidence, user.countryOfResidence) &&
                 Objects.equals(langKey, user.langKey) &&
                 Objects.equals(imageUrl, user.imageUrl) &&
-                Objects.equals(activationKey, user.activationKey) &&
-                Objects.equals(resetKey, user.resetKey) &&
-                Objects.equals(resetDate, user.resetDate) &&
                 Objects.equals(subscriptions, user.subscriptions) &&
                 Objects.equals(friends, user.friends) &&
                 Objects.equals(authority, user.authority);
@@ -229,6 +204,6 @@ public class User {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, login, password, firstName, lastName, email, activated, langKey, imageUrl, activationKey, resetKey, resetDate, subscriptions, friends, authority);
+        return Objects.hash(id, login, password, firstName, lastName, email, countryOfResidence, activated, langKey, imageUrl, subscriptions, friends, authority);
     }
 }
