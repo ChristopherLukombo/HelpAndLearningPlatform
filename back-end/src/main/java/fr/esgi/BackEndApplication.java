bdf0c8a7500d8ab6788c737891c0b05a903ceb04
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-04-14 00:23:18 +0200

    [updating main]

--- back-end/src/main/java/fr/esgi/BackEndApplication.java
+++ back-end/src/main/java/fr/esgi/BackEndApplication.java
@@ -1,13 +1,37 @@
 package fr.esgi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class BackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackEndApplication.class, args);
+	private static final Logger LOGGER = LoggerFactory.getLogger(BackEndApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(BackEndApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		LOGGER.info("\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\t{}://localhost:{}\n\t" +
+						"External: \t{}://{}:{}\t" +
+						"\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				protocol,
+				env.getProperty("server.port"),
+				protocol,
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
 
 }
