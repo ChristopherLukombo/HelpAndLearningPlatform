918738671f82e36222710b727d1544b5d8407ae1
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-05-26 18:01:18 +0200

    [fix bug: add annotation Param in repository]

--- back-end/src/main/java/fr/esgi/dao/NotationRepository.java
+++ back-end/src/main/java/fr/esgi/dao/NotationRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.esgi.domain.Notation;
@@ -15,5 +16,5 @@ import fr.esgi.domain.Notation;
 public interface NotationRepository extends JpaRepository<Notation, Long> {
 
     @Query("SELECT n FROM Notation n WHERE n.trick.id = :trickId")
-    List<Notation> findAllByTrickId(Long trickId);
+    List<Notation> findAllByTrickId(@Param("trickId") Long trickId);
 }
