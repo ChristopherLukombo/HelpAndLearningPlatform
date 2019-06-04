eb8a1af98f76bfa9089c2f5c27ba8a3d9427b887
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-06-04 13:45:43 +0200

    [services to get notations]

--- back-end/src/main/java/fr/esgi/service/NotationService.java
+++ back-end/src/main/java/fr/esgi/service/NotationService.java
@@ -1,7 +1,10 @@
 package fr.esgi.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import fr.esgi.exception.HelpAndLearningPlatformException;
 import fr.esgi.service.dto.NotationDTO;
 
 /**
@@ -16,4 +19,19 @@ public interface NotationService {
 	 * @return NotationDTO
 	 */
     NotationDTO save(NotationDTO notationDTO);
+    
+    /**
+     * Returns all notations by trickId
+     * @param trickId : id of trick
+     * @return list of entities
+     */
+    List<NotationDTO> findAllByTrickId(Long trickId);
+    
+    /**
+     * 
+     * @param id
+     * @return
+     * @throws HelpAndLearningPlatformException 
+     */
+    NotationDTO findOne(Long id) throws HelpAndLearningPlatformException;
 }
