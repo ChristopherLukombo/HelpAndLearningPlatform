3e76d212081ff38bf4adb12e004e2a21bac7da43
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-06-24 22:43:50 +0200

    [add crud for comments

--- back-end/src/main/java/fr/esgi/web/rest/CommentResource.java
+++ back-end/src/main/java/fr/esgi/web/rest/CommentResource.java
@@ -12,7 +12,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * REST controller for managing Comment.
@@ -40,27 +46,93 @@ public class CommentResource {
 	}
 
 	/**
-	 *  POST  /comments : save a comment.
+	 * POST  /comments : save a comment.
+	 * 
 	 * @param commentDTO the comment to create
 	 * @return the ResponseEntity with status 201 (Created) and with body the new comment.
 	 * @throws URISyntaxException if the Location URI syntax is incorrect
 	 */
 	@ApiOperation(value = "Save a comment.")
 	@ApiResponses(value = {
-	        @ApiResponse(code = 201, message = "Successful register"),
-	        @ApiResponse(code = 400, message = "Bad request"),
-	        @ApiResponse(code = 401, message = "Unauthorized"),
-	        @ApiResponse(code = 403, message = "Access denied"),
-	        })
+			@ApiResponse(code = 201, message = "Successful register"),
+			@ApiResponse(code = 400, message = "Bad request"),
+			@ApiResponse(code = 401, message = "Unauthorized"),
+			@ApiResponse(code = 403, message = "Access denied"),
+	})
 	@PostMapping("/comments")
 	public ResponseEntity<CommentDTO> createComment(@RequestBody @Valid CommentDTO commentDTO) throws URISyntaxException, HelpAndLearningPlatformException {
 		LOGGER.debug("REST request to save a comment: {}", commentDTO);
 		if (null != commentDTO.getId()) {
 			throw new HelpAndLearningPlatformException(HttpStatus.BAD_REQUEST.value(),
-					"A new comment cannot already have an ID");
+					"Un nouveau commentaire ne peut pas déjà avoir un id.");
 		}
 		final CommentDTO comment = commentService.save(commentDTO);
 		return ResponseEntity.created(new URI("/api/comments" + comment.getId()))
 				.body(comment);
+	}
+
+	/**
+	 * PUT  /comments : update a comment.
+	 * 
+	 * @param commentDTO the comment to update
+	 * @return the ResponseEntity with status 200 (Ok) and with body the updated comment.
+	 */
+	@PutMapping("/comments")
+	public ResponseEntity<CommentDTO> updateComment(@RequestBody @Valid CommentDTO commentDTO) throws HelpAndLearningPlatformException {
+		LOGGER.debug("REST request to update a comment: {}", commentDTO);
+		if (null == commentDTO.getId()) {
+			throw new HelpAndLearningPlatformException(HttpStatus.BAD_REQUEST.value(),
+					"Un commentaire doit avoir un id.");
+		}
+		final CommentDTO comment = commentService.update(commentDTO);
+		return ResponseEntity.ok()
+				.body(comment);
+	}
+
+	/**
+	 * GET  /comments : get all comments.
+	 * 
+	 * @return the ResponseEntity with status 200 (Ok) and with body the comments.
+	 */
+	@GetMapping("/comments")
+	public ResponseEntity<List<CommentDTO>> getAllComments() throws HelpAndLearningPlatformException {
+		LOGGER.debug("REST request to get all comments");
+		List<CommentDTO> commentsDTO = commentService.findAll();
+		if (commentsDTO.isEmpty()) {
+			throw new HelpAndLearningPlatformException(HttpStatus.NOT_FOUND.value(),
+					"Pas de commentaire");
+		}
+		return ResponseEntity.ok()
+				.body(commentsDTO);
+	}
+
+	/**
+	 * GET  /comments : get comment by id.
+	 * 
+	 * @return the ResponseEntity with status 200 (Ok) and with body the comment.
+	 */
+	@GetMapping("/comments/{id}")
+	public ResponseEntity<CommentDTO> getComment(@PathVariable Long id) throws HelpAndLearningPlatformException {
+		LOGGER.debug("REST request to get all comments");
+		Optional<CommentDTO> commentDTO = commentService.findOne(id);
+		if (commentDTO.isPresent()) {
+			return ResponseEntity.ok()
+					.body(commentDTO.get());
+		} else {
+			throw new HelpAndLearningPlatformException(HttpStatus.NOT_FOUND.value(),
+					"Pas de commentaire");
+		}
+	}
+
+	/**
+	 * DELETE  /comments : delete the comment by id.
+	 * 
+	 * @return the ResponseEntity with status 204 (No Content) and no comment.
+	 */
+	@DeleteMapping("/comments/{id}")
+	public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+		LOGGER.debug("REST request to delete comment : {}", id);
+		commentService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
